fe3169f4922929d82d5427cad97b66a2dec7b697
Shivam Amin <xShivam.Amin@gmail.com>
2018-01-15 01:58:31 -0500

    Fixed the HelpActivity
    The HelpActivity was not displaying correctly, so it was fixed and the layout was updated.

--- app/src/main/java/com/shivamamin/android/passwordmanager/HelpActivity.java
+++ app/src/main/java/com/shivamamin/android/passwordmanager/HelpActivity.java
@@ -1,16 +1,22 @@
 package com.shivamamin.android.passwordmanager;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 /**
  * Created by Shivam Amin on 2018-01-14.
  */
 
-public class HelpActivity extends Activity {
+public class HelpActivity extends AppCompatActivity {
     public static Intent newIntent(Context packageContext) {
         Intent intent = new Intent(packageContext, HelpActivity.class);
         return intent;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
     }
 }
--- app/src/main/java/com/shivamamin/android/passwordmanager/ServiceListFragment.java
+++ app/src/main/java/com/shivamamin/android/passwordmanager/ServiceListFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,7 +16,6 @@ import android.widget.TextView;
 import android.content.ClipboardManager;
 import android.content.ClipData;
 import android.content.Context;
-import android.widget.Toast;
 
 import java.util.List;
 
